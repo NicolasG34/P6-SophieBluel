7568a510fd1271e0817a0b2c671c77c8ead71cc8
Nicolas Gottiniaux <nicolas.gottiniaux@gmail.com>
2025-02-21 11:12:43 +0100

    ajout des photos par js et début d'ajout des filtres

---
+++ FrontEnd/script.js
@@ -0,0 +1,45 @@
+const apiURL = "http://localhost:5678/api/";
+
+fetch(apiURL + "works")
+.then((response) => { return response.json() })
+.then((data) => {
+    const gallery = document.querySelector(".gallery");
+    data.forEach(work => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const figCaption = document.createElement("figcaption");
+
+        image.src= work.imageUrl;
+        image.alt= work.title;
+        figCaption.innerHTML= work.title;
+
+        figure.appendChild(image);
+        figure.appendChild(figCaption);
+        gallery.appendChild(figure);
+    });
+    console.log(gallery);
+
+ })
+
+ fetch(apiURL + "categories")
+ .then((response) => { return response.json() })
+.then((data) => {
+    const categories = document.querySelector(".filters");
+    //const all = document.createElement("all");
+    data.forEach(category => {
+        const filter = document.createElement("div");
+        filter.classList.add("filters");
+        //filter.setAttribute("name", "filter");
+
+        //filter.src= category.name;
+        let names = filter.innerText;
+        names.src=category.name; 
+
+        categories.appendChild(filter);
+    });
+    console.log(categories);
+
+    
+
+})
+
